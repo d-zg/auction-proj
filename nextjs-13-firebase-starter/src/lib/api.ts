48dea23cc73f1eebbbc77a18e28d574a3f47e981
Daniel Zhang <danielzhang139@gmail.com>
2025-03-02 17:27:30 -0600

    read baseurl frontend from .env

--- nextjs-13-firebase-starter/src/lib/api.ts
+++ nextjs-13-firebase-starter/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiURL = 'http://localhost:8000' // Make sure this matches your backend URL
+const apiURL = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:8000'; // Read from env var, default to localhost
 export const api = axios.create({
     baseURL: apiURL,
     timeout: 10000,
